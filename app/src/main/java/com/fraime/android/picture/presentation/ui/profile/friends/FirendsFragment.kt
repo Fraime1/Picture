09501de8a44de76d117b0627a09c24771cbeddfb
master <you@example.com>
2022-06-29 18:08:30 +0700

    s

---
+++ app/src/main/java/com/fraime/android/picture/presentation/ui/profile/friends/FirendsFragment.kt
@@ -0,0 +1,6 @@
+package com.fraime.android.picture.presentation.ui.profile.friends
+
+import androidx.fragment.app.Fragment
+
+class FirendsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/fraime/android/picture/presentation/ui/profile/friends/FriendsViewModel.kt
@@ -0,0 +1,6 @@
+package com.fraime.android.picture.presentation.ui.profile.friends
+
+import androidx.lifecycle.ViewModel
+
+class FriendsViewModel :  ViewModel() {
+}
